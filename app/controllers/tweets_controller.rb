41735ee02b3a95e9fb095c127808d3c5cbb7831c
urielhdz <uriel.hdzc@gmail.com>
2023-07-26 16:40:58 -0600

    Changes

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -27,7 +27,7 @@ class TweetsController < ApplicationController
 
     respond_to do |format|
       if @tweet.save
-        format.html { redirect_to tweet_url(@tweet), notice: "Tweet was successfully created." }
+        format.html { redirect_back(fallback_location: root_path, notice: "Tweet was successfully created." ) }
         format.json { render :show, status: :created, location: @tweet }
       else
         format.html { render :new, status: :unprocessable_entity }
