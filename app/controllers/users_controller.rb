0753359f93f5648079e1a4fdec578766e26feaab
Eduardo Ismael García Pérez <eduardo78d@gmail.com>
2023-07-26 15:53:39 -0600

    links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   # GET /users/1 or /users/1.json
   def show
+    @tweets = @user.tweets
   end
 
   # GET /users/new
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   resources :retweets
   resources :hashtags
   resources :comments
-  resources :tweets
-  resources :users
+  resources :tweets do
+    post :retweet, on: :member 
+  end
+
+  resources :users 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
